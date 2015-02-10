0ce0b23d3e45ec86685fdcfd068dc9c7f36c674f
usrlocal <usrlocal@Gfixeetu120.ad.univ-angers.fr>
2015-02-10 14:48:47 +0100

    Code du bundle Entrainement commenté. Comme tous les bundles suivent ce même modèle, on ne commentera en détail que celui là

--- src/Basket/EntrainementBundle/Controller/EntrainementController.php
+++ src/Basket/EntrainementBundle/Controller/EntrainementController.php
@@ -11,18 +11,27 @@ class EntrainementController extends Controller
 {
     public function indexAction(Request $request)
     {
+        // On créé un nouvel entrainement vide
         $entrainement = new Entrainement();
+        // On créé un formulaire EntrainementType, qui hydratera notre entrainement
         $form = $this->createForm(new EntrainementType(),$entrainement);
+        // Le formulaire est complété par les données du POST
         $form->handleRequest($request);
+        // On vérifie que le formulaire est valide
         if ($form->isValid()){
+          // On utilise un Entity Manager
           $em = $this->getDoctrine()->getManager();
-          //$personne_repository = $em->getRepository('BasketDatabaseBundle:Personne');
+          // On sauvegarde notre entrainement en BDD
           $em->persist($entrainement);
           $em->flush();
         }
-        
+        // On récupère le contenu du repositoty EntrainementRepository, pour pouvoir effectuer des requêtes dessus
+        // et accéder aux données déjà sauvegardées en BDD
         $em = $this->getDoctrine()->getEntityManager()->getRepository('BasketDatabaseBundle:Entrainement');
+        // On récupère tous les entrainements
         $allEntrainement = $em->findAll();
+        // On retourne la vue index, à laquelle on passe en paramètres le formulaire ainsi que le tableau de tous les 
+        // entrainements
         return $this->render('BasketEntrainementBundle::index.html.twig', array(
             'form' => $form->createView(),
             'allEntrainement' => $allEntrainement,
